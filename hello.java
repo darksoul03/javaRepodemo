dbca8fc31674aedc0ff47c0ca74daaf51e9f6918
adityafamily0312 <69436445+adityafamily0312@users.noreply.github.com>
2020-08-23 15:38:05 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	  System.out.println("Amisha is Cute");
+	  System.out.println("Hello World");
 		
 	}
 
